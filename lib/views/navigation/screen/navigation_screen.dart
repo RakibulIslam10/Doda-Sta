3282ecd2ed7f5a58768431c36220a2f36728b678
rakib <rakib10.devs@gmail.com>
2025-08-31 19:29:32 +0600

    login screen done

--- lib/views/navigation/screen/navigation_screen.dart
+++ lib/views/navigation/screen/navigation_screen.dart
@@ -1,8 +1,14 @@
+import 'package:doda_work/core/themes/token.dart';
+import 'package:doda_work/core/utils/basic_import.dart';
 import 'package:doda_work/core/utils/extensions.dart';
+import 'package:doda_work/core/utils/space.dart';
+import 'package:doda_work/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import '../../../core/utils/dimensions.dart';
 import '../../../core/utils/layout.dart';
+import '../../../gen/assets.gen.dart';
 import '../controller/navigation_controller.dart';
 
 part 'navigation_screen_mobile.dart';

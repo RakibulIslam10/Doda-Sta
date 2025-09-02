d3cfb64f914e4e1918490e8618f2c34be14a21e7
rakib <rakib10.devs@gmail.com>
2025-09-02 11:57:27 +0600

    login screen done

--- lib/views/home/screen/home_screen_mobile.dart
+++ lib/views/home/screen/home_screen_mobile.dart
@@ -5,118 +5,36 @@ class HomeScreenMobile extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
-    final List<String> statusText = ['Pending', 'Ongoing', 'Complete'];
-
-    return DefaultTabController(
-      length: statusText.length,
-      child: Builder(
-        builder: (context) {
-          final TabController tabController = DefaultTabController.of(context);
-
-          // Swipe listener (instant color update)
-          tabController.animation!.addListener(() {
-            final int currentIndex = tabController.animation!.value.round();
-            if (controller.selectedStatus.value != currentIndex) {
-              controller.selectedStatus.value = currentIndex;
-            }
-          });
-
-          return Scaffold(
-            appBar: PreferredSize(
-              preferredSize: Size.fromHeight(Dimensions.appBarHeight * 1.25),
-              child: AppBar(
-                automaticallyImplyLeading: false,
-                flexibleSpace: const HomeAppBarWidgetView(),
-                actions: [
-                  Container(
-                    margin: Dimensions.defaultHorizontalSize.edgeRight,
-                    padding: EdgeInsets.all(Dimensions.paddingSize * 0.35),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(color: CustomColors.primary),
-                    ),
-                    child: SvgPicture.asset(Assets.icons.group),
-                  ),
-                  Space.width.v10,
-                ],
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(Dimensions.appBarHeight * 1.25),
+        child: AppBar(
+          automaticallyImplyLeading: false,
+          flexibleSpace: const HomeAppBarWidgetView(),
+          actions: [
+            Container(
+              margin: Dimensions.defaultHorizontalSize.edgeRight,
+              padding: EdgeInsets.all(Dimensions.paddingSize * 0.35),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: CustomColors.primary),
               ),
+              child: SvgPicture.asset(Assets.icons.group),
             ),
-            body: SafeArea(
-              child: ListView(
-                padding: Dimensions.defaultHorizontalSize.edgeHorizontal,
-                physics: NeverScrollableScrollPhysics(),
-                children: [
-                  const SearchBarWidgetView(),
-                  const CategoryWidgetView(),
-
-                  /// Custom Status TabBar
-                  Obx(
-                    () => TabBar(
-                      controller: tabController,
-                      tabAlignment: TabAlignment.start,
-                      isScrollable: true,
-                      indicatorColor: Colors.transparent,
-                      dividerColor: Colors.transparent,
-                      labelPadding: EdgeInsets.zero,
-                      enableFeedback: false,
-                      overlayColor: MaterialStateProperty.all(
-                        Colors.transparent,
-                      ),
-                      onTap: (value) => controller.selectedStatus.value = value,
-                      splashFactory: NoSplash.splashFactory,
-                      tabs: List.generate(statusText.length, (index) {
-                        final bool isSelected =
-                            controller.selectedStatus.value == index;
-                        return Container(
-                          margin: EdgeInsets.only(
-                            right: Dimensions.widthSize * 1.5,
-                          ),
-                          padding: EdgeInsets.symmetric(
-                            horizontal: Dimensions.widthSize * 1.2,
-                            vertical: Dimensions.verticalSize * 0.4,
-                          ),
-                          decoration: BoxDecoration(
-                            color: isSelected
-                                ? CustomColors.primary
-                                : CustomColors.disableColor,
-                            borderRadius: BorderRadius.circular(
-                              Dimensions.radius * 0.8,
-                            ),
-                          ),
-                          child: Center(
-                            child: TextWidget(
-                              statusText[index],
-                              fontWeight: FontWeight.w500,
-                              fontSize: Dimensions.titleSmall,
-                              color: isSelected
-                                  ? CustomColors.whiteColor
-                                  : CustomColors.blackColor,
-                            ),
-                          ),
-                        );
-                      }),
-                    ),
-                  ),
-
-                  Space.height.v10,
-
-                  /// TabBarView content
-                  SizedBox(
-                    height: 200,
-                    child: TabBarView(
-                      controller: tabController,
-                      children: [
-                        Center(child: TextWidget("Pending Content")),
-                        Center(child: TextWidget("Ongoing Content")),
-                        Center(child: TextWidget("Complete Content")),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          );
-        },
+            Space.width.v10,
+          ],
+        ),
+      ),
+      body: SafeArea(
+        child: ListView(
+          padding: Dimensions.defaultHorizontalSize.edgeHorizontal,
+          physics: NeverScrollableScrollPhysics(),
+          children: [
+            const SearchBarWidgetView(),
+            const CategoryWidgetView(),
+            const StatusWidgetView(),
+          ],
+        ),
       ),
     );
   }
--- lib/views/home/widget/status_widget.dart
+++ lib/views/home/widget/status_widget.dart
@@ -1,6 +1,6 @@
 part of '../screen/home_screen.dart';
 
-class StatusWidgetView extends StatelessWidget {
+class StatusWidgetView extends GetView<HomeController> {
   const StatusWidgetView({super.key});
 
   @override
@@ -9,30 +9,101 @@ class StatusWidgetView extends StatelessWidget {
 
     return DefaultTabController(
       length: statusText.length,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
+      child: Builder(
+        builder: (context) {
+          final TabController tabController = DefaultTabController.of(context);
 
+          /// Swipe listener (instant color update)
+          tabController.animation!.addListener(() {
+            final int currentIndex = tabController.animation!.value.round();
+            if (controller.selectedStatus.value != currentIndex) {
+              controller.selectedStatus.value = currentIndex;
+            }
+          });
 
-          // Space.height.v10,
-          // SizedBox(
-          //   height: MediaQuery.of(context).size.height,
-          //   child: ListView.builder(
-          //     itemBuilder: (context, index) {
-          //       return Container(
-          //         margin: EdgeInsetsGeometry.only(
-          //           bottom: Dimensions.verticalSize * 0.5,
-          //         ),
-          //         height: MediaQuery.of(context).size.height * 0.15,
-          //         decoration: BoxDecoration(
-          //           color: CustomColors.primary,
-          //           borderRadius: BorderRadius.circular(Dimensions.radius),
-          //         ),
-          //       );
-          //     },
-          //   ),
-          // ),
-        ],
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /// Custom Status TabBar
+              Obx(
+                () => TabBar(
+                  controller: tabController,
+                  tabAlignment: TabAlignment.start,
+                  isScrollable: true,
+                  indicatorColor: Colors.transparent,
+                  dividerColor: Colors.transparent,
+                  labelPadding: EdgeInsets.zero,
+                  enableFeedback: false,
+                  overlayColor: MaterialStateProperty.all(Colors.transparent),
+                  onTap: (value) => controller.selectedStatus.value = value,
+                  splashFactory: NoSplash.splashFactory,
+                  tabs: List.generate(statusText.length, (index) {
+                    final bool isSelected =
+                        controller.selectedStatus.value == index;
+                    return Container(
+                      margin: EdgeInsets.only(
+                        right: Dimensions.widthSize * 1.5,
+                      ),
+                      padding: EdgeInsets.symmetric(
+                        horizontal: Dimensions.widthSize * 1.2,
+                        vertical: Dimensions.verticalSize * 0.32,
+                      ),
+                      decoration: BoxDecoration(
+                        color: isSelected
+                            ? CustomColors.primary
+                            : CustomColors.primary.withAlpha(858),
+                        borderRadius: BorderRadius.circular(
+                          Dimensions.radius * 0.8,
+                        ),
+                      ),
+                      child: Center(
+                        child: TextWidget(
+                          statusText[index],
+                          fontWeight: FontWeight.w500,
+                          fontSize: Dimensions.titleSmall,
+                          color: isSelected
+                              ? CustomColors.whiteColor
+                              : CustomColors.blackColor,
+                        ),
+                      ),
+                    );
+                  }),
+                ),
+              ),
+              Space.height.v10,
+
+              /// TabBarView content
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                child: TabBarView(
+                  controller: tabController,
+                  children: List.generate(statusText.length, (index) {
+                    return ListView.builder(
+                      physics: ClampingScrollPhysics(),
+                      cacheExtent: 500,
+                      shrinkWrap: true,
+                      itemCount: 10,
+                      itemBuilder: (context, itemIndex) {
+                        return Container(
+                          margin: EdgeInsets.only(
+                            bottom: Dimensions.verticalSize * 0.5,
+                          ),
+                          height: MediaQuery.of(context).size.height * 0.15,
+                          decoration: BoxDecoration(
+                            color: CustomColors.primary,
+                            borderRadius: BorderRadius.circular(
+                              Dimensions.radius,
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }),
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }

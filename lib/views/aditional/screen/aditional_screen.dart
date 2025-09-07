c61b26e15ba87173e7f3eb3696974d63f888cb07
rakib <rakib10.devs@gmail.com>
2025-09-07 13:06:17 +0600

    category ongoing

--- lib/views/aditional/screen/aditional_screen.dart
+++ lib/views/aditional/screen/aditional_screen.dart
@@ -5,6 +5,7 @@ import 'package:doda_work/core/utils/extensions.dart';
 import '../../../widgets/custom_drop_down_widget.dart';
 import '../../../widgets/time_picker_widget.dart';
 import '../controller/aditional_controller.dart';
+
 part 'aditional_screen_mobile.dart';
 
 class AditionalScreen extends GetView<AditionalController> {

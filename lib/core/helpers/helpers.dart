99e93d5b75090775a8c68281e07807a5dd8e2acc
RakibulIslam10 <rakibulislam10.net@gmail.com>
2025-10-19 15:16:45 +0600

    APi Start

--- lib/core/helpers/helpers.dart
+++
@@ -1,7 +0,0 @@
-class Helpers {
-  static String? confirmPasswordValidator(String? value, String? password) {
-    if (value == null || value.isEmpty) return 'Please confirm your password';
-    if (value != password) return 'Passwords do not match';
-    return null;
-  }
-}
--- lib/views/splash/controller/splash_controller.dart
+++ lib/views/splash/controller/splash_controller.dart
@@ -7,7 +7,7 @@ class SplashController extends GetxController {
   void onReady() {
     super.onReady();
     Future.delayed(Duration(seconds: 1), () {
-      Get.offAllNamed(Routes.onboardScreen);
+      Get.offAllNamed(Routes.loginScreen);
     });
   }
 }

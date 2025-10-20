b2c7d087d36d625997752c6dfb903d853906ba9a
RakibulIslam10 <rakibulislam10.net@gmail.com>
2025-10-20 13:24:45 +0600

    APi

--- lib/views/splash/controller/splash_controller.dart
+++ lib/views/splash/controller/splash_controller.dart
@@ -7,7 +7,7 @@ class SplashController extends GetxController {
   void onReady() {
     super.onReady();
     Future.delayed(Duration(seconds: 1), () {
-      Get.offAllNamed(Routes.loginScreen);
+      Get.offAllNamed(Routes.welcomeScreen);
     });
   }
 }

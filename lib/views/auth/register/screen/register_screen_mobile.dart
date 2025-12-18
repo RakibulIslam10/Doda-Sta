610ac6972c20252a76f8971123d1913a91a7166c
RakibulIslam10 <rakibulislam10.net@gmail.com>
2025-12-18 14:51:18 +0600

    terms and link

--- lib/views/auth/register/screen/register_screen_mobile.dart
+++ lib/views/auth/register/screen/register_screen_mobile.dart
@@ -15,7 +15,6 @@ class RegisterScreenMobile extends GetView<RegisterController> {
           children: [
             Space.height.v10,
             TextWidget(
-
               'Sign Up',
               fontSize: Dimensions.titleLarge,
               fontWeight: FontWeight.bold,
@@ -60,51 +59,4 @@ class RegisterScreenMobile extends GetView<RegisterController> {
   }
 }
 
-class ShakeWidget extends StatefulWidget {
-  final Widget child;
 
-  const ShakeWidget({super.key, required this.child});
-
-  @override
-  State<ShakeWidget> createState() => _ShakeWidgetState();
-}
-
-class _ShakeWidgetState extends State<ShakeWidget>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _offsetAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      duration: const Duration(milliseconds: 400),
-      vsync: this,
-    );
-    _offsetAnimation = Tween(
-      begin: 0.0,
-      end: 8.0,
-    ).chain(CurveTween(curve: Curves.elasticIn)).animate(_controller);
-    _controller.forward();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _offsetAnimation,
-      builder: (context, child) {
-        return Transform.translate(
-          offset: Offset(_offsetAnimation.value, 0),
-          child: child,
-        );
-      },
-      child: widget.child,
-    );
-  }
-}
--- lib/views/auth/register/widget/button_and_text_section.dart
+++ lib/views/auth/register/widget/button_and_text_section.dart
@@ -1,4 +1,6 @@
-import 'package:doda_work/core/utils/basic_import.dart';
+
+import '../../../../core/utils/basic_import.dart';
+import '../../../../widgets/web_vew_page_widget.dart';
 import '../controller/register_controller.dart';
 
 class ButtonAndTextSectionView extends GetView<RegisterController> {
@@ -9,53 +11,30 @@ class ButtonAndTextSectionView extends GetView<RegisterController> {
     return Column(
       children: [
         Space.height.v20,
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 24,
-              width: 24,
-              child: Obx(
-                () => Checkbox(
-                  activeColor: CustomColors.primary,
-                  side: BorderSide(
-                    color: CustomColors.disableColor,
-                    width: 1.4,
-                  ),
-                  value: controller.isCheck.value,
-                  onChanged: (value) {
-                    controller.isCheck.value = !controller.isCheck.value;
-                    controller.isError.value = false;
-                  },
-                ),
+        TermsAndPolicyWidget(
+          isChecked: controller.isCheck,
+          isError: controller.isError,
+          mainText: 'DodaWork',
+          termsTap: () {
+            Get.to(() => const WebViewScreen(
+              url: 'https://flutter.dev/',
+              title: 'Terms & Conditions',
+            ),
+            );
+          },
+          policyTap: () {
+            Get.to(
+                  () => const WebViewScreen(
+                url: 'https://flutter.dev/',
+                title: 'Privacy Policy',
               ),
-            ),
-            Expanded(
-              child: Obx(() {
-                return Transform.translate(
-                  key: ValueKey(controller.isError.value),
-                  offset: controller.isError.value
-                      ? const Offset(10, 0)
-                      : Offset.zero,
-                  child: TextWidget(
-                    onTap: () {
-                      controller.isCheck.value = !controller.isCheck.value;
-                      controller.isError.value = false;
-                    },
-                    padding: EdgeInsets.only(left: Dimensions.widthSize),
-                    "I have read and agree to dodawork's Terms and Conditions and Policy.",
-                    maxLines: 2,
-                    fontSize: Dimensions.titleSmall * 0.9,
-                    color: controller.isError.value ? Colors.red : Colors.grey,
-                  ),
-                );
-              }),
-            ),
-          ],
+            );
+          },
         ),
+
         Space.height.v20,
         Obx(
-          () => PrimaryButtonWidget(
+              () => PrimaryButtonWidget(
             isLoading: controller.isLoading.value,
             title: "Next",
             onPressed: () {
@@ -71,6 +50,119 @@ class ButtonAndTextSectionView extends GetView<RegisterController> {
           ),
         ),
       ],
+    );
+  }
+}
+
+
+
+class TermsAndPolicyWidget extends StatelessWidget {
+  final RxBool isChecked;
+  final RxBool isError;
+  final String mainText;
+  final VoidCallback termsTap;
+  final VoidCallback policyTap;
+
+  const TermsAndPolicyWidget({
+    super.key,
+    required this.isChecked,
+    required this.isError,
+    required this.mainText,
+    required this.termsTap,
+    required this.policyTap,
+  });
+
+  void _toggle() {
+    isChecked.value = !isChecked.value;
+    if (isChecked.value) {
+      isError.value = false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final primaryColor = Theme.of(context).primaryColor;
+    const errorColor = Colors.red;
+    const normalColor = Colors.grey;
+
+    return InkWell(
+      onTap: _toggle,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Obx(
+            () => SizedBox(
+              height: 24.h,
+              width: 24.w,
+              child: Checkbox(
+                value: isChecked.value,
+                activeColor: primaryColor,
+
+                side: BorderSide(
+                  color: isError.value ? errorColor : CustomColors.disableColor,
+                  width: 1.4.w,
+                ),
+                onChanged: (_) => _toggle(), 
+              ),
+            ),
+          ),
+          Space.width.v10,
+          Expanded(
+            child: Obx(
+              () => AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
+                transform: Matrix4.translationValues(
+                  isError.value ? 10 : 0,
+                  0,
+                  0,
+                ),
+                child: Wrap(
+                  children: [
+                    _text(
+                      "I have read and agree to $mainText's ",
+                      color: isError.value ? errorColor : normalColor,
+                    ),
+                    _link(
+                      "Terms & Conditions",
+                      termsTap,
+                      isError.value ? errorColor : primaryColor,
+                    ),
+                    _text(
+                      " and ",
+                      color: isError.value ? errorColor : normalColor,
+                    ),
+                    _link(
+                      "Privacy Policy",
+                      policyTap,
+                      isError.value ? errorColor : primaryColor,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // Normal text
+  Widget _text(String text, {required Color color}) {
+    return Text(text, style: TextStyle(fontSize: 14, color: color));
+  }
+
+  // Clickable link text
+  Widget _link(String text, VoidCallback onTap, Color color) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Text(
+        text,
+        style: TextStyle(
+          fontSize: 14,
+          color: color,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
     );
   }
 }

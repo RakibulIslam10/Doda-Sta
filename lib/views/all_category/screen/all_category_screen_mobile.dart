5e453ce96da0713bdb8f4e08a1b3abefc18798da
RakibulIslam10 <rakibulislam10.net@gmail.com>
2026-02-03 11:00:20 +0600

    category

--- lib/views/all_category/screen/all_category_screen_mobile.dart
+++ lib/views/all_category/screen/all_category_screen_mobile.dart
@@ -64,7 +64,7 @@ class AllCategoryScreenMobile extends GetView<AllCategoryController> {
                   ClipOval(
                     child: CachedNetworkImage(
                       imageUrl: category.icon != null
-                          ? ApiEndPoints.baseUrl + (category.icon ?? "")
+                          ?  (category.icon ?? "")
                           : "https://picsum.photos/200/300?random=$index",
                       width: screenWidth * 0.16,
                       height: screenWidth * 0.16,

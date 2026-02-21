6855bc1faf993949a198f49698985fc407a98a72
RakibulIslam10 <rakibulislam10.net@gmail.com>
2026-02-21 12:22:30 +0600

    request status updated

--- lib/views/vendor_profile/screen/vendor_profile_screen_mobile.dart
+++ lib/views/vendor_profile/screen/vendor_profile_screen_mobile.dart
@@ -235,6 +235,8 @@ class VendorProfileScreenMobile extends GetView<VendorProfileController> {
                         },
                       ),
                     ),
+                    Space.height.betweenInputBox,
+
                   ],
                 ),
         ),
@@ -242,38 +244,4 @@ class VendorProfileScreenMobile extends GetView<VendorProfileController> {
     );
   }
 
-  // void _openPicker(BuildContext context) async {
-  //   await Navigator.push(
-  //     context,
-  //     MaterialPageRoute(
-  //       builder: (context) => MapLocationPicker(
-  //         config: MapLocationPickerConfig(
-  //           apiKey: "AIzaSyC_qKHmzl-HHB9hr8-fWGmhETSVR2H0894",
-  //           onNext: (result) {
-  //             if (result != null &&
-  //                 result.geometry?.location.lat != null &&
-  //                 result.geometry?.location.lat != null) {
-  //               controller.selectedLatLng.value = LatLng(
-  //                 result.geometry!.location.lat,
-  //                 result.geometry!.location.lng,
-  //               );
-  //
-  //               controller.selectedAddress.value =
-  //                   result.formattedAddress ?? "";
-  //             }
-  //             if (Navigator.canPop(context)) {
-  //               Navigator.pop(context);
-  //             }
-  //           },
-  //         ),
-  //         geoCodingConfig: GeoCodingConfig(
-  //           apiKey: "AIzaSyC_qKHmzl-HHB9hr8-fWGmhETSVR2H0894",
-  //         ),
-  //         searchConfig: SearchConfig(
-  //           apiKey: "AIzaSyC_qKHmzl-HHB9hr8-fWGmhETSVR2H0894",
-  //         ),
-  //       ),
-  //     ),
-  //   );
-  // }
 }
